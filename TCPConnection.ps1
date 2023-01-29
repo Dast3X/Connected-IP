36e4a46006074503be89ba089ea5bf77e801623f
Dast3X <65315002+Dast3X@users.noreply.github.com>
2023-01-29 18:13:03 +0200

    Add files via upload

---
+++ TCPConnection.ps1
@@ -0,0 +1,52 @@
+﻿function Get-Location($get){
+$request
+$ips = @()
+foreach($i in $get){
+$i
+$ips += Invoke-RestMethod -Method Get -Uri "http://ip-api.com/json/$i"
+}
+echo "`n"
+$ips | Format-Table -Property @{Name="Ip Addres";Expression={$_.query}}, city, country, isp | Out-String|% {Write-Host $_}
+}
+
+while(1){
+cls
+$option = Read-Host "`t1 - Port
+`t2 - Ip
+`t3 - List all connections
+`nOption "
+if($option -eq 1)
+  {
+     cls
+     $port = Read-Host "Port "
+     cls
+     $get = Get-NetTCPConnection -LocalPort $port
+     echo $get
+     Get-Location $get.LocalAddress
+     Read-Host
+  }
+elseif($option -eq 2)
+  {
+     cls
+     $Ip = Read-Host "Ip "
+     cls
+     $get = Get-NetTCPConnection -LocalAddress $Ip
+     echo $get
+     Get-Location $get.LocalAddress
+     Read-Host
+  }
+elseif($option -eq 3)
+  {
+  cls
+     $get = Get-NetTCPConnection
+     echo $get
+     Read-Host
+  }
+elseif($option -eq "exit")
+  {
+  exit
+  }
+else{
+    continue
+}
+}
